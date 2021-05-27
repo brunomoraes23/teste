b7d4497841b73986e18c9de8fcdf99ed08f7547e
Bruno Moraes <bruno.moraes23@hotmail.com>
2021-05-27 13:52:45 -0300

    finished project version 1.0

---
+++ app/Jobs/SendMessages.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use Illuminate\Support\Facades\Http;
+use App\Models\Receiver;
+
+class SendMessages implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $receivers = Receiver::where('status', 'Ativo')->get();
+
+        $feed = Http::get('https://g1.globo.com/rss/g1/');
+        $xml = simplexml_load_string($feed);
+        
+        $titles = '';
+        foreach($xml->channel->item as $item){
+            $titles .= strval($item->title) ."\n\n";
+        }
+
+        $messages = [];
+        foreach($receivers as $receiver){
+            $message = 
+                $receiver->name
+                ."\n\n"
+                .$titles;
+            $obj = [];
+            $obj['message'] = $message;
+            $obj['phone'] = $receiver->phone;
+            $obj["test_mode"] = true;
+            array_push($messages, $obj);
+        }
+
+        $response = Http::withHeaders([
+            'Authorization' => env('APP_AUTHORIZATION', 'Bearer pmzR55tvXIcUBWBY9lEsJqa6xarIyrA1e2VYAgG4viGWKZkRfcW8bwm7ZbUg')
+        ])->post('https://zapito.com.br/api/messages', [
+            'test_mode' => true,
+            'data' => $messages
+        ]);
+    }
+}
